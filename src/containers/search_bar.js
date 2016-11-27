269df01e5825ec7b169d4c4ba7412bdca9cec76b
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-27 17:14:29 +0100

    Edit searchbar component to have a controlled field

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' }
+
+    // Callback function that references a function with "this" needs to bind to the context
+    this.onInputChange = this.onInputChange.bind(this)
+  }
+
+  // all DOM event handlers (onChange, onClick, onHover...) come along with the (event) object.
+  onInputChange(event) {
+    console.log(event.target.value)
+    this.setState({ term: event.target.value })
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={ this.state.term }
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Search</button>
         </span>
