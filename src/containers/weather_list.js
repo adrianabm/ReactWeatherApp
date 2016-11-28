5e57c2bba80f072c4ecaefeb2b8183d943db5d6d
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-28 10:30:05 +0100

    Add awesome charts for temperature

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Sparklines, SparklinesLine } from 'react-sparklines'
 
 class WeatherList extends Component {
+  // renderWeather is to render a single city
   renderWeather(cityData) {
     const name = cityData.city.name
+    const temps = cityData.list.map(weather => weather.main.temp)
 
     return (
-      <tr>
+      <tr key={ name }>
+        <td>{ name }</td>
         <td>
-          { name }
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
         </td>
       </tr>
     )
@@ -16,10 +22,7 @@ class WeatherList extends Component {
 
   render() {
     return (
-      <table
-        className="table table-hover"
-        key={ name }
-        >
+      <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
